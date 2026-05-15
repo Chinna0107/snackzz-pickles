667f86dd4b2e5192c106b8fe5b166a0074352803
chinna0107 <kancharlahemanth89@gmail.com>
2026-05-15 21:10:56 +0530

    production 2.23

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,7 +83,7 @@ export const metadata: Metadata = {
     canonical: "https://snakzee.com",
   },
   verification: {
-    google: "",
+    google: "-_TathQN_9RE9b6qKYjx70TeZCfIrGGIBQwEzM344Yw",
   },
 };
 
